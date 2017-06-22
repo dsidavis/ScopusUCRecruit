f644c3f6d84cdfa0227f47bb8de7f5dd2c8dfa3d
Duncan Temple Lang <duncan@r-project.org>
2017-06-22 12:58:10 -0700

    code to get the SGR id from the id column

---
+++ sgr.R
@@ -0,0 +1,16 @@
+d = data.frame(id = c("1234", "5678;10000", "no;yes", "123;456;789"),
+               idType = c("SGR", "SGR;DOI", "DOI;SGR", "DOI;FOO;BAR"),
+               stringsAsFactors = FALSE)
+
+getSGR = 
+function(df)
+{
+   type = strsplit(df$idType, ";")
+   id = strsplit(df$id, ";")
+   ans = mapply(function(x, types) {
+                 names(x) = types
+                 x[ "SGR" ]
+                },
+                id, type)
+   ans
+}
